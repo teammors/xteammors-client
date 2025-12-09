d7e26ba065785c2e254a24c116aeaacc28385064
Teammors <teammorsservice@gmail.com>
2025-12-09 17:56:13 +0800

    D 54%

--- lib/views/chat/reply_header.dart
+++ lib/views/chat/reply_header.dart
@@ -15,75 +15,78 @@ class ReplyHeader extends StatelessWidget {
     final bg = isDark ? Colors.white12 : Colors.black12;
     final Color textColor = isMe ? Colors.white : cs.onSurfaceVariant;
 
-    Widget? media;
-    String label;
-    switch (type) {
-      case MessageType.image:
-        label = 'Photo';
-        media = ClipRRect(
-          borderRadius: BorderRadius.circular(4),
-          child: SizedBox(
-            width: 40,
-            height: 40,
-            child: thumbUrl != null
-                ? Image.network(
-                    thumbUrl!,
-                    fit: BoxFit.cover,
-                    loadingBuilder: (c, child, p) => p == null
-                        ? child
-                        : Center(
-                            child: SizedBox(width: 16, height: 16, child: CircularProgressIndicator(strokeWidth: 2))),
-                    errorBuilder: (c, e, s) => ColoredBox(
-                      color: Colors.grey[800]!,
-                      child: const Icon(Icons.broken_image, size: 16),
-                    ),
-                  )
-                : const ColoredBox(color: Colors.grey),
-          ),
-        );
-        break;
-      case MessageType.video:
-        label = 'Video';
-        media = Stack(
-          alignment: Alignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(4),
-              child: SizedBox(
-                width: 40,
-                height: 40,
-                child: thumbUrl != null
-                    ? Image.network(
-                        thumbUrl!,
-                        fit: BoxFit.cover,
-                        loadingBuilder: (c, child, p) => p == null
-                            ? child
-                            : Center(
-                                child: SizedBox(width: 16, height: 16, child: CircularProgressIndicator(strokeWidth: 2))),
-                        errorBuilder: (c, e, s) => ColoredBox(
-                          color: Colors.grey[800]!,
-                          child: const Icon(Icons.broken_image, size: 16),
-                        ),
-                      )
-                    : const ColoredBox(color: Colors.black12),
-              ),
+  Widget? media;
+  String label;
+  switch (type) {
+    case MessageType.text:
+      label = 'Text';
+      break;
+    case MessageType.image:
+      label = 'Photo';
+      media = ClipRRect(
+        borderRadius: BorderRadius.circular(4),
+        child: SizedBox(
+          width: 40,
+          height: 40,
+          child: thumbUrl != null
+              ? Image.network(
+                  thumbUrl!,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (c, child, p) => p == null
+                      ? child
+                      : Center(
+                          child: SizedBox(width: 16, height: 16, child: CircularProgressIndicator(strokeWidth: 2))),
+                  errorBuilder: (c, e, s) => ColoredBox(
+                    color: Colors.grey[800]!,
+                    child: const Icon(Icons.broken_image, size: 16),
+                  ),
+                )
+              : const ColoredBox(color: Colors.grey),
+        ),
+      );
+      break;
+    case MessageType.video:
+      label = 'Video';
+      media = Stack(
+        alignment: Alignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: SizedBox(
+              width: 40,
+              height: 40,
+              child: thumbUrl != null
+                  ? Image.network(
+                      thumbUrl!,
+                      fit: BoxFit.cover,
+                      loadingBuilder: (c, child, p) => p == null
+                          ? child
+                          : Center(
+                              child: SizedBox(width: 16, height: 16, child: CircularProgressIndicator(strokeWidth: 2))),
+                      errorBuilder: (c, e, s) => ColoredBox(
+                        color: Colors.grey[800]!,
+                        child: const Icon(Icons.broken_image, size: 16),
+                      ),
+                    )
+                  : const ColoredBox(color: Colors.black12),
             ),
-            const Icon(Icons.play_arrow, size: 16, color: Colors.white),
-          ],
-        );
-        break;
-      case MessageType.voice:
-        label = 'Voice message';
-        break;
-      case MessageType.file:
-        label = 'File';
-        break;
-      case MessageType.emoji:
-        label = 'Emoji';
-        break;
-      default:
-        label = preview ?? '';
-    }
+          ),
+          const Icon(Icons.play_arrow, size: 16, color: Colors.white),
+        ],
+      );
+      break;
+    case MessageType.voice:
+      label = 'Voice message';
+      break;
+    case MessageType.file:
+      label = 'File';
+      break;
+    case MessageType.emoji:
+      label = 'Emoji';
+      break;
+    default:
+      label = preview ?? '';
+  }
 
     return Container(
       padding: const EdgeInsets.all(8),
